e1a0e067c1f2ca1549325a1ca5429ad63b9ba754
Chris Jannenga <cjannenga312@gmail.com>
2019-10-18 22:44:41 -0700

    Added a material library

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthCardComponent } from './auth-card/auth-card.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [AuthCardComponent],
+  exports: [AuthCardComponent]
 })
 export class UiModule {}
